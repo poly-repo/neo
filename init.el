4988277709a86aba6bf5906251b7b581f48958e5
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-06 11:53:05 -0400

    A better init.el commentary
    This is mainly for testing copybara, as it might very well bit rotten
    
    GitOrigin-RevId: 732df03ef1f0f0c3380ace62101205a9d585a9c0

--- init.el
+++ init.el
@@ -1,9 +1,15 @@
-;;; init.el --- Where the magic begins ✨
+;;; neo --- My Emacs configuration, in its N-th reincarnation
+;;; -*- lexical-binding: t; -*-
 
-;; This is the beginning of a beautiful config. 
+;;; Commentary:
+
+;;; Where the magic begins ✨ [well, that would be early-init.el 🤪]
+;;; It is what I like to use. Most likely is not what you'd like to
+;;; use and vscode might serve you better.
+
 ;; Abandon all hope, ye who enter uncustomized.
 
-;; Warning: Side effects may include productivity, editor superiority,
-;; and unsolicited evangelism about Emacs.
 
-;; Let there be `(setq)!`
+;;; Code:
+
+
