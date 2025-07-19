8ecdea60915188a0150957aafcbe41dffb6bc380
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-19 11:07:36 -0400

    Refactor functions needed in early-init.el
    We give them a separate file so that they can be required in both
    early-init and init.
    
    GitOrigin-RevId: dfe987070a30bf929f122e7ae732fdf675bc41ba

---
+++ core/early-init-utils.el
@@ -0,0 +1,27 @@
+(require 'cl-lib)
+
+(defvar neo/--emacs-instance-name-cache nil
+  "Cached result of `neo/get-emacs-instance-name`.")
+
+(defun neo/get-emacs-instance-name ()
+  "Return the Emacs instance name from `--name`, or fallback to `emacs`.
+
+Reads from /proc/self/cmdline if needed (Linux-only)."
+  (or neo/--emacs-instance-name-cache
+      (setq neo/--emacs-instance-name-cache
+            (or
+             ;; Try $EMACS_NAME for compatibility or scripting
+             (getenv "EMACS_NAME")
+             ;; Try /proc/self/cmdline
+             (when (and (eq system-type 'gnu/linux)
+                        (file-readable-p "/proc/self/cmdline"))
+               (with-temp-buffer
+                 (insert-file-contents-literally "/proc/self/cmdline")
+                 (let* ((args (split-string (buffer-string) "\0" t))
+                        (index (cl-position "--name" args :test #'string=)))
+                   (when index
+                     (nth (1+ index) args)))))
+             ;; Fallback
+             "emacs"))))
+
+(provide 'early-init-utils)
