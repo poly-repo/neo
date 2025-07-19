cbf0fef9f6094501c14d27a2ed8b60616a9b8593
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-19 11:10:43 -0400

    Visit all neo .el files
    This is temp only, but for now is useful to have all files available
    in the Emacs we run for testing. Later desktop-mode will take care of this.
    
    GitOrigin-RevId: 510321a23e417d1c015243bc5a21f1a79da431c2

--- init.el
+++ init.el
@@ -12,4 +12,28 @@
 
 ;;; Code:
 
+;;; NOTE: we keep the eln cache out of here as we need to set it very
+;;; early in early-init.el and I didn't want to duplicate the
+;;; definitions of the no-littering directories. Natively compiled
+;;; modules are in
+;;; XDG_CONFIG_HOME/.cache/neo/eln-cache/emacs-neo-devel-emacs-31.0
+;;; TODO check if that .cache subdir is ok for XDG naming.
+(defvar no-littering-etc-directory (expand-file-name ".litter/config" user-emacs-directory))
+(defvar no-littering-var-directory (expand-file-name ".litter/data" user-emacs-directory))
 
+(dolist (subdir '("core"))
+  (add-to-list 'load-path (expand-file-name subdir user-emacs-directory)))
+
+(require 'early-init-utils)
+(require 'neo)
+
+;;; TODO temp only, but is useful to have access to all NEO .el file
+;;; from the dev Emacs we launch for testing things
+(defun neo/visit-all-elisp-files (&optional dir)
+  "Recursively visit all .el files under DIR (default: `user-emacs-directory`)."
+  (let ((root (file-name-as-directory (or dir user-emacs-directory))))
+    (dolist (file (directory-files-recursively root "\\.el\\'"))
+      (find-file-noselect file))))
+(setq vc-follow-symlinks t)
+(setq enable-local-variables :all)
+(neo/visit-all-elisp-files)
