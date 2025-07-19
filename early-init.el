dca50253b9b391043d6c91e7857d444d4d01a3f4
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-18 21:53:29 -0400

    Cleanup of early-initel
    Not nearly as clean as it needs to be but it's a start.
    
    GitOrigin-RevId: bc8161707e137cf664ecaff56d21f71d5547cf11

--- early-init.el
+++ early-init.el
@@ -7,112 +7,79 @@
 
 ;;; Code:
 
-;  ╭────────────────────────────────────────────────────────────────────────────────────────
-;  │ :gear: Early Init/General
-;  ╰────────────────────────────────────────────────────────────────────────────────────────
-
 (setq package-enable-at-startup nil)
-
 (setq native-comp-async-report-warnings-errors nil)
 
-;  ╭────────────────────────────────────────────────────────────────────────────────────────
-;  │ :gear: Early Init/UI
-;  ╰────────────────────────────────────────────────────────────────────────────────────────
-
 ;;; TODO: I don't seem to be able to change initial-frame-alist (for the most part we use .Xdefaults, so not a big deal)
-
-(defun neo/default-frame-parameters (params)
-  "Add each parameter in PARAMETERS to `default-frame-alist`."
-  (dolist (param params)
-    (push param initial-frame-alist)
-    (push param default-frame-alist)))
+;;; maybe it is lexical scoping at play
+;; (defun neo/default-frame-parameters (params)
+;;   "Add each parameter in PARAMETERS to `default-frame-alist`."
+;;   (dolist (param params)
+;;     (push param initial-frame-alist)
+;;     (push param default-frame-alist)))
   
-;; set/inhibit UI config to cure startup flickering
-;;; TODO: many of these go in init.el
-;;; TODO: cannot get rid of the message on how to get information about the GNU system
-(setq
- frame-inhibit-implied-resize t
- frame-resize-pixelwise t
- inhibit-splash-screen t
- inhibit-startup-buffer-menu t
- inhibit-startup-echo-area-message user-login-name
- inhibit-startup-message t
- inhibit-startup-screen t
- menu-bar-mode nil
- ring-bell-function 'ignore
- scroll-bar-mode nil
- tool-bar-mode nil
- use-dialog-box t
- use-file-dialog nil
- use-short-answers t
- )
+;; ;; set/inhibit UI config to cure startup flickering
+;; ;;; TODO: many of these go in init.el
+;; ;;; TODO: cannot get rid of the message on how to get information about the GNU system
+;; (setq
+;;  frame-inhibit-implied-resize t
+;;  frame-resize-pixelwise t
+;;  inhibit-splash-screen t
+;;  inhibit-startup-buffer-menu t
+;;  inhibit-startup-echo-area-message user-login-name
+;;  inhibit-startup-message t
+;;  inhibit-startup-screen t
+;;  menu-bar-mode nil
+;;  ring-bell-function 'ignore
+;;  scroll-bar-mode nil
+;;  tool-bar-mode nil
+;;  use-dialog-box t
+;;  use-file-dialog nil
+;;  use-short-answers t
+;;  )
 
-(set-scroll-bar-mode nil)
-(tooltip-mode -1)
+;; (set-scroll-bar-mode nil)
+;; (tooltip-mode -1)
 
-(neo/default-frame-parameters '(
-                                (width . 800)
-                                (height . 900)))
+;; (neo/default-frame-parameters '(
+;;                                 (width . 800)
+;;;                                 (height . 900)))
 
-;  ╭────────────────────────────────────────────────────────────────────────────────────────
-;  │ :gear: Early Init/Save Areas
-;  ╰────────────────────────────────────────────────────────────────────────────────────────
-
-;;; matching setting in init.el
-(setq no-littering-etc-directory (expand-file-name ".litter/config" user-emacs-directory))
-(setq no-littering-var-directory (expand-file-name ".litter/data" user-emacs-directory))
-
-;; Set eln-cache dir
-;; [per no-littering reccommendations: https://github.com/emacscollective/no-littering?tab=readme-ov-file#native-compilation-cache]
-(when (boundp 'native-comp-eln-load-path)
-  (startup-redirect-eln-cache (expand-file-name "eln-cache/" no-littering-var-directory)))
+(add-to-list 'load-path
+             (expand-file-name "core" (file-name-directory (or load-file-name buffer-file-name))))
+(require 'early-init-utils)
 
 
-;  ╭────────────────────────────────────────────────────────────────────────────────────────
-;  │ :gear: Early Init/Packages
-;  ╰────────────────────────────────────────────────────────────────────────────────────────
+(startup-redirect-eln-cache
+ (expand-file-name
+  (format "eln-cache/%s-emacs-%d.%d/"
+          (neo/get-emacs-instance-name)
+          emacs-major-version
+          emacs-minor-version)
+  (expand-file-name "neo" (or (getenv "XDG_CONFIG_HOME") "~/.cache"))))
 
-;; Not necessarily belongs here, but allows us to keep init.el cleaner
-;; NOTE: the snippet on the elpaca website uses user-emacs-directory. When replacing this with
-;; a new version remember to change that to no-littering-var-directory
-;; (see '<==')
-(defvar elpaca-installer-version 0.11)
-(defvar elpaca-directory (expand-file-name "elpaca/" no-littering-var-directory)) ; <==
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1 :inherit ignore
-                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (<= emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-                                                  ,@(when-let* ((depth (plist-get order :depth)))
-                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
-                                                  ,(plist-get order :repo) ,repo))))
-                  ((zerop (call-process "git" nil buffer t "checkout"
-                                        (or (plist-get order :ref) "--"))))
-                  (emacs (concat invocation-directory invocation-name))
-                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                  ((require 'elpaca))
-                  ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
+(let* ((instance-name (neo/get-emacs-instance-name))
+       (early-config (expand-file-name
+                      (format "%s-early-init-config.el" instance-name)
+                      (expand-file-name "neo" (or (getenv "XDG_CONFIG_HOME") "~/.config")))))
+  (message (format "instance name: %s" instance-name))
+  (message (format "config: %s [%s]" early-config (file-readable-p early-config)))
+  (when (file-readable-p early-config)
+    (message (format "Reading %s" early-config))
+    (load early-config nil 'nomessage)))
+
+(setq gc-cons-threshold (* 100 1000 1000))
+(setq inhibit-compacting-font-caches t)
+
+;;; Emacs insists on disabling the advertisement for themselves in the
+;;; echo area unless (setq inhibit-startup-echo-area-message USERNAME)
+;;; textually appears in init.el. For some reason they don't look in
+;;; early-init.el, hence we nuke everything.
+(if neo/nuke-echo-area-message
+    (progn
+      (defun startup-echo-area-message () "")
+      (defun display-startup-echo-area-message () nil)))
+
+
 
 
