06276391ac7a31d93971b21bbedb4c38ce2036af
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-10-06 11:44:19 -0400

    refactor: Adjust module loading and disable neo-extensions
    GitOrigin-RevId: d8a4c0d9ce02702d9bb375e1b88726b7daa59a83

--- core/neo.el
+++ core/neo.el
@@ -2,14 +2,14 @@
   (message "Requiring %s" feature)
   (require feature))
 
-(neo--require 'neo-struct)
-
 (neo--require 'neo-custom)			; customization groups
 (neo--require 'neo-utils)			; utility functions
 (neo--require 'neo-config)
 (neo--require 'neo-packages)
 
-(neo--require 'neo-extensions)
+(neo--require 'neo-struct)
 (neo--require 'neo-extensions-fetch)
+
+;(neo--require 'neo-extensions)
 
 (provide 'neo)
