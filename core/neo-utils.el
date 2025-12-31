ed869e388e17ea31f81879bfcd4fb6269790070f
Maurizio Vitale <109253047+m-vitale@users.noreply.github.com>
2025-12-30 23:08:15 -0500

    ✨ feat(neo): Tell Emacs how to get to the source code for builtin functions (#150)
    Closes: #148
    GitOrigin-RevId: c8ac17b1a6cc236a633e22e4ba42f5ef80fa9a43

--- core/neo-utils.el
+++ core/neo-utils.el
@@ -1,6 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
-;; TODO this will go into utility
+(defun neo/set-emacs-source-directories ()
+  (let (src)
+    ;; Prefer configure-time prefix
+    (when (and system-configuration-options
+               (string-match "--prefix=\\([^ ]+\\)" system-configuration-options))
+      (setq src (expand-file-name
+                 "src/"
+                 (match-string 1 system-configuration-options))))
+    ;; Fallback: runtime invocation
+    (unless (and src (file-directory-p src))
+      (setq src (expand-file-name
+                 "src/"
+                 (file-name-directory
+                  (directory-file-name invocation-directory)))))
+    (when (file-directory-p src)
+      (setq source-directory src
+            find-function-C-source-directory src))))
+
+
 (defun neo/with-ui-session (name &optional setup-fn teardown-fn)
   "Start a temporary UI session called NAME.
 
--- core/neo.el
+++ core/neo.el
@@ -5,18 +5,19 @@
 (defvar neo/use-extensions  t
   "When t we use neo extension mechanism. nil make immediate use of use-package")
 
-
 (defun neo--require (feature)
   "A wrapper around `require` for FEATURE."
   (require feature))
 
+(neo--require 'neo-utils)
 (neo--require 'neo-custom)			; customization groups
 (neo--require 'neo-packages)			; package management
 (neo--require 'neo-logging)
 (neo--require 'neo-framework)			; neo infra and core extension management
 
-(setq neo--framework (neo--framework-instance))
+(neo/set-emacs-source-directories)
 
+(setq neo--framework (neo--framework-instance))
 (neo/bootstrap neo--framework)
 
 (provide 'neo)
