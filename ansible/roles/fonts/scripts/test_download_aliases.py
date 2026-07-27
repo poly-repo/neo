a6ae86d120f45376547c276e41dfd40bbb310822
Maurizio Vitale <mrz.vtl+github@gmail.com>
2026-07-27 16:18:37 -0400

    Fix Iosevka font archive downloads
    Use the smaller Nerd Fonts tar.xz release artifacts, stream them through unique temporary files, and safely extract font files in both download surfaces.
    
    Beads: omega-3q16.24
    GitOrigin-RevId: 0c56e875b25b1b06fee9eb82f030603a2fd0f941

--- ansible/roles/fonts/scripts/test_download_aliases.py
+++ ansible/roles/fonts/scripts/test_download_aliases.py
@@ -13,23 +13,25 @@ def load_module(name, path):
 
 
 class TestNerdFontAssetAliases(unittest.TestCase):
-    def test_ansible_downloader_maps_fontawesome_to_symbols_only(self):
+    def test_ansible_downloader_uses_compressed_release_archives(self):
         module = load_module(
             'fonts_role_download',
             Path('infra/ansible/roles/fonts/scripts/download'),
         )
 
         self.assertEqual(module.nerd_release_asset_name('FontAwesome'), 'NerdFontsSymbolsOnly')
-        self.assertEqual(module.nerd_release_asset_name('Hack'), 'Hack')
+        self.assertEqual(module.nerd_release_archive_name('FontAwesome'), 'NerdFontsSymbolsOnly.tar.xz')
+        self.assertEqual(module.nerd_release_archive_name('Iosevka'), 'Iosevka.tar.xz')
 
-    def test_standalone_fetcher_maps_fontawesome_to_symbols_only(self):
+    def test_standalone_fetcher_uses_compressed_release_archives(self):
         module = load_module(
             'font_fetcher_download',
             Path('infra/tools/font_fetcher/o-font-fetcher'),
         )
 
         self.assertEqual(module.nerd_release_asset_name('FontAwesome'), 'NerdFontsSymbolsOnly')
-        self.assertEqual(module.nerd_release_asset_name('Hack'), 'Hack')
+        self.assertEqual(module.nerd_release_archive_name('FontAwesome'), 'NerdFontsSymbolsOnly.tar.xz')
+        self.assertEqual(module.nerd_release_archive_name('Iosevka'), 'Iosevka.tar.xz')
 
 
 if __name__ == '__main__':
